0924ed15501e46506e08995739c624a04a087d93
eighthday <gareth@8dd.co.uk>
2016-09-29 10:25:39 +0100

    With svg map

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var active = document.getElementById('active')
+var menuBtns
+var isFirst = false
+
+var menu = document.getElementById('menu')
+var isOpen = false
+var en = document.getElementById('en-go')
+
+
+active.innerHTML = "England"
+en.className = " active";
+
+document.getElementById('menu-toggle').addEventListener('click', function(e) {
+    if (isOpen == false) {
+        menu.className = " show";
+        isOpen = true
+    } else {
+        closeMenu()
+    }
+
+}, false);
+
+menuBtns = menu.getElementsByTagName('a');
+
+for (var i = 0; i < menuBtns.length; i++) {
+    menuBtns[i].addEventListener('click', setActive, false);
+
+}
+
+function setActive() {
+
+    for (var i = 0; i < menuBtns.length; i++) {
+        menuBtns[i].className = " "
+    }
+
+    active.innerHTML = this.innerHTML;
+    this.className = " active"
+    closeMenu()
+}
+
+function closeMenu() {
+    menu.className = " hide";
+    isOpen = false
+}
