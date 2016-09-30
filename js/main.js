16f768bf5a93c8ec26fb154df4f5a13667851b74
eighthday <gareth@8dd.co.uk>
2016-09-30 20:22:19 +0100

    js ops

--- js/main.js
+++ js/main.js
@@ -1,25 +1,6 @@
-
-
-var menuBtns
-
 var active = document.getElementById('active')
 active.innerHTML = "England"
-
-var menu = document.getElementById('menu')
-
-
-document.getElementById('menu-toggle').addEventListener('click', function(e) {
-
-  menu.className = " show";
-}, false);
-
 menuBtns = menu.getElementsByTagName('a');
-
 for (var i = 0; i < menuBtns.length; i++) {
-  menuBtns[i].addEventListener('click', setActive, false);
-}
-
-function setActive() {
-  active.innerHTML = this.innerHTML;
-  menu.className = " hide";
+  menuBtns[i].addEventListener('click', function() {active.innerHTML = this.innerHTML; }, false);
 }
