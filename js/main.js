7ef53c9daf9095877786e08485c7e09b630bc9ac
eighthday <gareth@8dd.co.uk>
2016-09-30 15:24:44 +0100

    file saving

--- js/main.js
+++ js/main.js
@@ -1,39 +1,30 @@
 var active = document.getElementById('active')
-
 var menuBtns
 active.innerHTML = "England"
-var isFirst = false
 
 var menu = document.getElementById('menu')
 var isOpen = false
 var en = document.getElementById('en-go')
 en.className = " active";
+
 document.getElementById('menu-toggle').addEventListener('click', function(e) {
-e.preventDefault()
-    menu.className = " show";
-
-
+  e.preventDefault()
+  menu.className = " show";
 }, false);
 
 menuBtns = menu.getElementsByTagName('a');
 
 for (var i = 0; i < menuBtns.length; i++) {
   menuBtns[i].addEventListener('click', setActive, false);
-
 }
 
 function setActive() {
-
   for (var i = 0; i < menuBtns.length; i++) {
     menuBtns[i].className = " "
   }
 
   active.innerHTML = this.innerHTML;
   this.className = " active"
-  closeMenu()
-}
-
-function closeMenu() {
   menu.className = " hide";
   isOpen = false
 }
